0f08577faf65ed897428870c27afd74e6ce57235
Alexander <partizan.pc7@gmail.com>
2022-10-04 14:51:05 +0300

    first debug tg

---
+++ src/app/index.tsx
@@ -0,0 +1,21 @@
+import React, {useEffect} from 'react';
+
+const tg = window.Telegram.WebApp;
+
+function App() {
+  useEffect(() => {
+    tg.ready();
+  }, []);
+
+  const onClose = () => {
+    tg.close();
+  }
+
+  return (
+    <div>
+      <button onClick={onClose}>Закрыть</button>
+    </div>
+  );
+}
+
+export default App;
